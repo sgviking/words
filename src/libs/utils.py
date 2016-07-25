b8ff0b61b7119bc46ecb4e8c496272b27c0d113c
Dan Daggett <dan@socialgeeks.com>
2016-07-25 15:50:33 +0000

    Add comment to count_words() function in src/libs/utils.py

--- src/libs/utils.py
+++ src/libs/utils.py
@@ -4,9 +4,15 @@ import string
 
 
 def count_words(text):
-    # words = re.split(ur'\W+', text.lower())
-    # words = filter(None, words)
+    '''
+    Takes a string, counts the total number of words and the occurence
+    of each word within the spring.  Ignores punctuation.
 
+    Note: This will count numbers as a word.  For dealing with very large
+    strings in a production environment this could be optimized by processing
+    the strings in pieces.  Since this string comes from a POST request
+    proxied in from NGINX this is limited by the size of allowed POST bodies.
+    '''
     # regex from here: http://stackoverflow.com/a/12705513
     # match words, including words with apostrophes. Remove line endings
     words = re.split(ur"(\w[\w']*\w|\w)", text.lower().replace('\n', ' '))
